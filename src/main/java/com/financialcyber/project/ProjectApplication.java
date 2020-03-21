b2b147aa0c7af9ff229c6fb2fe8ee2f9841d0fc2
CatalinaR <rotarucatalina991@gmail.com>
2020-03-21 12:13:06 +0000

    Initial commit-POC

---
+++ src/main/java/com/financialcyber/project/ProjectApplication.java
@@ -0,0 +1,68 @@
+package com.financialcyber.project;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	private static final String API_key ="";
+
+	public static List<String> getTradingData(String stockName) throws IOException, ParseException {
+		String urlName_base = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
+		String urlName_complete = urlName_base + stockName + "&apikey=" + API_key;
+		URL url = new URL(urlName_complete);
+		List<String> result = new ArrayList<>();
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.connect();
+		int responsecode = conn.getResponseCode();
+		if(responsecode!=200){
+			throw new RuntimeException("HTTP status " + responsecode);
+		} else{
+			//JSONParser json = new JSONParser();
+			System.out.println(responsecode);
+			System.out.println(conn.getResponseMessage());
+
+			String inline = "";
+			Scanner sc = new Scanner(url.openStream());
+			while(sc.hasNext())
+			{
+				inline+=sc.nextLine();
+			}
+			System.out.println("\nJSON data in string format");
+			System.out.println(inline);
+			sc.close();
+
+			JSONParser parse = new JSONParser();
+			Object jsonObj = parse.parse(inline);
+			JSONObject obj = (JSONObject) jsonObj;
+			System.out.println(obj.get("Meta Data"));
+		}
+
+
+		return result;
+	}
+
+
+	public static void main(String[] args) throws IOException, ParseException {
+		SpringApplication.run(ProjectApplication.class, args);
+		List<String> result = getTradingData("AAPL");
+
+	}
+
+
+}
