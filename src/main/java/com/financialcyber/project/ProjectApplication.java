8352bc34a3347a8fc42b04d4cf6a53cbd11314a0
CatalinaR <rotarucatalina991@gmail.com>
2020-03-30 07:51:18 +0100

    "Implementation for processing the Guardian news json"

--- src/main/java/com/financialcyber/project/ProjectApplication.java
+++ src/main/java/com/financialcyber/project/ProjectApplication.java
@@ -7,23 +7,36 @@ import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.print.DocFlavor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.SQLOutput;
 import java.util.*;
 
 @SpringBootApplication
 public class ProjectApplication {
 
 	private static final Map<String, String> env = System.getenv();
-	private static final String API_key =  env.get("API_ALPHA_VANTAGE");
+	private static final String API_key_alpha =  env.get("API_ALPHA_VANTAGE");
+	private static final String API_key_guardian =  env.get("API_GUARDIAN");
 
-	public static JSONObject getJsonTradingData(String stockName, String function) throws IOException, ParseException {
+
+	public static String constructURLNews(String pageNumber){
+		String urlName_base = "http://content.guardianapis.com/search?q=data-computer-security&section=technology&page="+pageNumber;
+		return urlName_base + "&api-key=" + API_key_guardian;
+	}
+
+	public static String constructURLStocks(String function, String stockName){
 		String urlName_base = "https://www.alphavantage.co/query?function="+ function + "&symbol=";
-		String urlName_complete = urlName_base + stockName + "&apikey=" + API_key;
-		URL url = new URL(urlName_complete);
+		return  urlName_base + stockName + "&apikey=" + API_key_alpha;
+	}
+
+
+	public static JSONObject getURLData(String urlName) throws IOException, ParseException {
+		URL url = new URL(urlName);
 		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.connect();
@@ -32,23 +45,18 @@ public class ProjectApplication {
 		if(responsecode!=200){
 			throw new RuntimeException("HTTP status " + responsecode);
 		} else{
-			//System.out.println(responsecode);
-			//System.out.println(conn.getResponseMessage());
-
 			String inline = "";
 			Scanner sc = new Scanner(url.openStream());
 			while(sc.hasNext())
 			{
 				inline+=sc.nextLine();
 			}
-			//System.out.println("\nJSON data in string format");
-			//System.out.println(inline);
+
 			sc.close();
 
 			JSONParser parse = new JSONParser();
 			Object jsonObj = parse.parse(inline);
 			objectReturn = (JSONObject) jsonObj;
-			//System.out.println(objectReturn.get("Meta Data"));
 		}
 		return objectReturn;
 	}
@@ -66,8 +74,14 @@ public class ProjectApplication {
 					metadata.put("timezone", subEntries[1]);
 				} else if(subEntries[0].contains("Symbol")){
 					metadata.put("symbol", subEntries[1]);
-				} else if(subEntries[0].contains("Last Refreshed"));
-					metadata.put("lastRefreshDate", subEntries[1]);
+				} else if(subEntries[0].contains("Last Refreshed")) {
+					int len = subEntries[1].length();
+					if (subEntries[1].charAt(len - 1) == '}') {
+						metadata.put("lastRefreshDate", subEntries[1].substring(0, len - 1));
+					} else {
+						metadata.put("lastRefreshDate", subEntries[1]);
+					}
+				}
 			}
 		}
 		return metadata;
@@ -108,13 +122,41 @@ public class ProjectApplication {
 	}
 
 
+	public static Map<Integer, Map<String, String>> processNewsMetaData(JSONObject obj) throws ParseException {
+		Map<Integer, Map<String, String>> result = new HashMap<>();
+		String response = obj.get("response").toString();
+		JSONParser parse = new JSONParser();
+		JSONObject jsonObj = (JSONObject) parse.parse(response);
+		String resutsList = jsonObj.get("results").toString();
+		JSONArray resultListObj = (JSONArray) parse.parse(resutsList);
+		for(int i = 0; i<resultListObj.size(); i++){
+			Map<String, String> setResultJson = new HashMap<>();
+			JSONObject elementObj = (JSONObject) parse.parse(resultListObj.get(i).toString());
+			setResultJson.put("webPublicationDate", elementObj.get("webPublicationDate").toString());
+			setResultJson.put("webUrl", elementObj.get("webUrl").toString());
+			setResultJson.put("webTitle", elementObj.get("webTitle").toString());
+			setResultJson.put("id", elementObj.get("id").toString());
+			result.put(i+1, setResultJson);
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) throws IOException, ParseException {
 		SpringApplication.run(ProjectApplication.class, args);
-		JSONObject obj = getJsonTradingData("AAPL", "TIME_SERIES_DAILY");
+		JSONObject obj = getURLData(constructURLStocks("TIME_SERIES_DAILY", "AAPL"));
 		Map<String, String> processR = processJsonMetadata(obj);
 		processR.forEach((a, b) -> System.out.println(a + " " + b));
 		Map<String, Map<String, String>> result = processJsonStocks(obj);
 		System.out.println(result.toString());
+
+		JSONObject obj2 = getURLData(constructURLNews("1"));
+		Map<Integer, Map<String, String>> proceessR = processNewsMetaData(obj2);
+		for(Integer elem : proceessR.keySet()){
+			Map<String, String> e = proceessR.get(elem);
+			e.forEach((a,b) -> System.out.println(a + "   " + b));
+		}
+
 	}
 
 
