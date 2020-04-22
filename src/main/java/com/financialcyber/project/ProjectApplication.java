6f884948ec478709fb49c4d4949646cb6fbd9dbc
CatalinaR <rotarucatalina991@gmail.com>
2020-04-22 21:46:05 +0100

    Write to file method for SQL queries

--- src/main/java/com/financialcyber/project/ProjectApplication.java
+++ src/main/java/com/financialcyber/project/ProjectApplication.java
@@ -2,6 +2,7 @@ package com.financialcyber.project;
 
 
 import com.financialcyber.project.services.CaptureDataImpl;
+import com.financialcyber.project.services.InsertDataImpl;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
@@ -19,11 +20,13 @@ public class ProjectApplication  {
 
 	public static void main(String[] args) throws IOException, ParseException {
 		SpringApplication.run(ProjectApplication.class, args);
-		JSONObject obj = getURLData(CaptureDataImpl.constructURLStocks("TIME_SERIES_DAILY", "AAPL"));
-		CaptureDataImpl.processJsonMetadata(obj);
+//		JSONObject obj = getURLData(CaptureDataImpl.constructURLStocks("TIME_SERIES_DAILY", "AAPL"));
+//		CaptureDataImpl.processJsonMetadata(obj);
+//
+//		JSONObject obj2 = CaptureDataImpl.getURLData(CaptureDataImpl.constructURLNews("1"));
+//		CaptureDataImpl.processNewsMetaData(obj2);
 
-		JSONObject obj2 = CaptureDataImpl.getURLData(CaptureDataImpl.constructURLNews("1"));
-		CaptureDataImpl.processNewsMetaData(obj2);
+		//InsertDataImpl.printToFile();
 
 	}
 
--- src/main/java/com/financialcyber/project/services/InsertDataImpl.java
+++ src/main/java/com/financialcyber/project/services/InsertDataImpl.java
@@ -3,9 +3,29 @@ package com.financialcyber.project.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Connection;
 
 public class InsertDataImpl {
+
+    public static StringBuffer constructSQLStock(){
+        return new StringBuffer();
+    }
+
+
+
+    public static void printToFile() throws IOException {
+        StringBuffer bufferString = new StringBuffer();
+        bufferString.append("INSERT; INSERT");
+        FileOutputStream fos = new FileOutputStream("C:/Users/catal/Desktop/FinancialCyberProject/FinancialCyberAnalyser/src/main/resources/DML.sql");
+        DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(fos));
+        outStream.writeUTF(String.valueOf(bufferString));
+        outStream.close();
+
+    }
 
 
 
