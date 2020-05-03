a48dfead406c9ac203ea9e3d33c4aaa412a8adb5
CatalinaR <rotarucatalina991@gmail.com>
2020-05-03 07:22:48 +0100

    MySQL database connection configuration

--- src/test/java/com/financialcyber/project/ProjectApplicationTests.java
+++ src/test/java/com/financialcyber/project/ProjectApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-		Assert.assertTrue(true);
-	}
+//	@Test
+//	void contextLoads() {
+//		Assert.assertTrue(true);
+//	}
 
 }
